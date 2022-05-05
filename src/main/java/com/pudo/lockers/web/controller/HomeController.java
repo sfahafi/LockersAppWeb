7064b9c45f053bfea6b01aee41346b892e755e06
Sharbel <sfahafi18@gmail.com>
2022-05-05 12:33:33 -0300

    Test MVC DB Inicial

--- src/main/java/com/pudo/lockers/web/controller/HomeController.java
+++ src/main/java/com/pudo/lockers/web/controller/HomeController.java
@@ -1,16 +1,26 @@
 package com.pudo.lockers.web.controller;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.pudo.lockers.web.interfaces.I_TerminalInterface;
+import com.pudo.lockers.web.model.Terminal;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private I_TerminalInterface terminal;
+	
 	@GetMapping("/tabla")
-	public String mostrarTabla() {
-
+	public String listaTerminales(Model model) {
+		List<Terminal> lista = terminal.buscarTodas();
+		model.addAttribute("terminales", lista);
 		return "listTerm";
 	}
 	
